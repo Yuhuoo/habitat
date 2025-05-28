faa02e98e9c9c485977d42c3e2a1cd287a6a3ea3
aogao <dhugaoao@163.com>
2025-05-28 21:46:27 +0800

    feat: support scanet preprocess

--- client.sh
+++ client.sh
@@ -1,12 +1,11 @@
 
-input=data/scanet/high_res/scene0001_00/scene0001_00_vh_clean.ply
-output=data/scanet/high_res/scene0000_00/scene0000_00_vh_clean.glb
-# assimp export $input $output
+scene=data/scanet/high_res/scans/scene0006_00/scene0006_00_vh_clean.glb
+saved_path=data/scanet/high_res/scans/scene0006_00
 python viewer_client.py \
-    --scene $output \
+    --scene $scene \
     --data data/scanet/high_res/scene_dataset_config.json \
     --width 960 --height 640 \
-    --saved_path test_output/scanet_action_5.27 \
+    --saved_path $saved_path \
     --start_position "[2.2184856, 1.6997461, -1.5994138]" \
     --start_rotation "[0.983254909515381, 0, -0.182235538959503, 0]" \
     --sensor_height 1.5 
