721deaee69bb4ca9daa0fcae9b9bf2159c53c4ef
Yuhuoo <dhugaoao@163.com>
2025-06-14 00:32:55 +0800

    feat: support habitat->scanet point cloud coordindate tranform

--- server.sh
+++ server.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-# python src/viewer.py \
-#     --scene data/scene_datasets/oLBMNvg9in8/oLBMNvg9in8.glb \
-#     --dataset data/scene_datasets/oLBMNvg9in8/mp3d.scene_dataset_config.json \
-#     --action_path data/scene_datasets/oLBMNvg9in8/5_26.txt \
-#     --output_path output/matterport3d/oLBMNvg9in8/5_26_2 \
-#     --feq 3
+# python src/viewer_server.py \
+#     --scene data/datasets/Scannet/scans/scene0046_00/scene0046_00_vh_clean.glb \
+#     --dataset data/datasets/Scannet/scene_dataset_config.json \
+#     --action_path data/datasets/Scannet/scans/scene0046_00/action.txt \
+#     --output_path output/scanet/test/scene0046_00 \
+#     --start_position "2.7847664,  1.5457658, -2.013033" \
+#     --start_rotation "0.769103646278381, -0.111514739692211, 0.622807800769806, 0.0903028398752213" \
+#     --sensor_height 0.0 \
+#     --feq 4
+
+# python src/viewer_server.py \
+#     --scene data/datasets/Scannet/scans/scene0050_00/scene0050_00_vh_clean.glb \
+#     --dataset data/datasets/Scannet/scene_dataset_config.json \
+#     --action_path data/datasets/Scannet/scans/scene0050_00/action.txt \
+#     --output_path output/scanet/test/scene0050_00 \
+#     --start_position "5.3083987,  1.8769357,  -1.778661" \
+#     --start_rotation "-0.86167573928833, 0.105547100305557, -0.49615478515625, -0.0143296699970961" \
+#     --sensor_height 0.0 \
+#     --feq 4
 # zip -q -r output/matterport3d/oLBMNvg9in8/second.zip output/matterport3d/oLBMNvg9in8/second
 
+# python src/viewer_server.py \
+#     --scene data/datasets/Scannet/scans/scene0084_00/scene0084_00_vh_clean.glb \
+#     --dataset data/datasets/Scannet/scene_dataset_config.json \
+#     --action_path data/datasets/Scannet/scans/scene0084_00/action.txt \
+#     --output_path output/scanet/test/scene0084_00 \
+#     --start_position "0.13359478,  0.7007719,  -1.9164732" \
+#     --start_rotation "0.382067292928696, -0.0216953512281179, -0.922393977642059, -0.0523773208260536" \
+#     --sensor_height 0.0 \
+#     --feq 4
+# zip -q -r output/matterport3d/oLBMNvg9in8/second.zip output/matterport3d/oLBMNvg9in8/second
+   
 # data_dir="data/datasets/Scannet/scans"
-# scene="scene0084_00"
+# scene="scene0000_00"
 # echo "===================${scene} Begin============================="
 # glb_path="$data_dir/$scene/${scene}_vh_clean.glb"
 # config_path="$data_dir/../scene_dataset_config.json"
-# obs_saved_path="output/scanet/new/$scene"
+# obs_saved_path="output/scanet/test/$scene"
 # echo "Using scene: $glb_path"
 # echo "Using config: $config_path"
 # echo "Output will be saved to: $obs_saved_path"
@@ -21,26 +45,28 @@
 #     --scene $glb_path \
 #     --dataset $config_path \
 #     --output_path $obs_saved_path \
-#     --sensor_height 0.7
+#     --sensor_height 0.7 \
+#     --width 256 \
+#     --height 256
 # echo "===================== ${scene} Done==========================="
 
-# data_dir="data/datasets/Scannet/scans"
-# for scene in $(ls $data_dir); do
-#     echo "===================${scene} Begin============================="
-#     glb_path="$data_dir/$scene/${scene}_vh_clean.glb"
-#     config_path="$data_dir/../scene_dataset_config.json"
-#     obs_saved_path="output/scanet/new/$scene"
-#     echo "Using scene: $glb_path"
-#     echo "Using config: $config_path"
-#     echo "Output will be saved to: $obs_saved_path"
-#     python src/viewer_server.py \
-#         --scene $glb_path \
-#         --dataset $config_path \
-#         --output_path $obs_saved_path \
-#         --sensor_height 0.7
-#     echo "===================== ${scene} Done==========================="
-#     # zip -q -r output/scanet/new.zip output/scanet/new
-# done
+data_dir="data/datasets/Scannet/scans"
+for scene in $(ls $data_dir); do
+    echo "===================${scene} Begin============================="
+    glb_path="$data_dir/$scene/${scene}_vh_clean.glb"
+    config_path="$data_dir/../scene_dataset_config.json"
+    obs_saved_path="output/scanet/new/$scene"
+    echo "Using scene: $glb_path"
+    echo "Using config: $config_path"
+    echo "Output will be saved to: $obs_saved_path"
+    python src/viewer_server.py \
+        --scene $glb_path \
+        --dataset $config_path \
+        --output_path $obs_saved_path \
+        --sensor_height 0.7
+    echo "===================== ${scene} Done==========================="
+    # zip -q -r output/scanet/new.zip output/scanet/new
+done
 
 
 
